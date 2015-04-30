b4dc42fac82b3645bbcde2ed5a9f1fad1bb18e77
David Collom <david.collom@williamhill.com>
2015-04-30 11:27:06 +0100

    Removal of labels code - not meant to be included in PR

--- lib/marathon/app.rb
+++ lib/marathon/app.rb
@@ -3,8 +3,7 @@
 class Marathon::App < Marathon::Base
 
   ACCESSORS = %w[ id args cmd cpus disk env executor instances mem ports requirePorts
-                  storeUris tasksHealthy tasksUnhealthy tasksRunning tasksStaged upgradeStrategy
-                  uris user version labels ]
+                  storeUris tasksHealthy tasksUnhealthy tasksRunning tasksStaged upgradeStrategy uris user version ]
 
   DEFAULTS = {
     :env => {},
