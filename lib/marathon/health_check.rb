6e298ddc9fa5ddb186282b59c5026c8e8208fe68
Imri Zvik <imrizvik@gmail.com>
2017-01-12 12:23:36 +0200

    Fixes #45 - Health check should allow HTTPS as the protocol
    Also add MESOS_* health checks

--- lib/marathon/health_check.rb
+++ lib/marathon/health_check.rb
@@ -13,20 +13,21 @@ class Marathon::HealthCheck < Marathon::Base
   }
 
   ACCESSORS = %w[ command gracePeriodSeconds intervalSeconds maxConsecutiveFailures
-                  path portIndex protocol timeoutSeconds ]
+                  path portIndex protocol timeoutSeconds ignoreHttp1xx ]
 
   # Create a new health check object.
   # ++hash++: Hash returned by API.
   def initialize(hash)
     super(Marathon::Util.merge_keywordized_hash(DEFAULTS, hash), ACCESSORS)
-    Marathon::Util.validate_choice(:protocol, protocol, %w[HTTP TCP COMMAND])
+    Marathon::Util.validate_choice(:protocol, protocol, %w[HTTP TCP COMMAND HTTPS MESOS_HTTP MESOS_HTTPS MESOS_TCP])
   end
 
   def to_s
     if protocol == 'COMMAND'
       "Marathon::HealthCheck { :protocol => #{protocol} :command => #{command} }"
-    elsif protocol == 'HTTP'
-      "Marathon::HealthCheck { :protocol => #{protocol} :portIndex => #{portIndex} :path => #{path} }"
+    elsif %w[HTTP HTTPS MESOS_HTTP MESOS_HTTPS].include? protocol
+      "Marathon::HealthCheck { :protocol => #{protocol} :portIndex => #{portIndex} :path => #{path}" +
+          (%w[HTTP HTTPS].include? protocol and !ignoreHttp1xx.nil? ? " :ignoreHttp1xx => #{ignoreHttp1xx}" : '') + " }"
     else
       "Marathon::HealthCheck { :protocol => #{protocol} :portIndex => #{portIndex} }"
     end
